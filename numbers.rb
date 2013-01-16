59fde2c20aedd0e21258dd53adc716201904453f
johndavid400 <jd@isotope11.com>
2013-01-16 16:16:52 -0600

    break up output

--- numbers.rb
+++ numbers.rb
@@ -3,15 +3,19 @@ class Compute
 
   def initialize(input)
     @input = input.to_f
-    if input_is_square?
-      puts "Indeed"
-    else
-      puts "Abysmal!"
-    end
+    input_is_square? ? print_result : warn_of_failure
   end
 
   def input_is_square?
     Math.sqrt(@input).to_i == Math.sqrt(@input)
+  end
+
+  def warn_of_failure
+    print "The number you entered is not a perfect square, please try again."
+  end
+
+  def print_result
+    puts @input.to_i
   end
 
 end
@@ -19,3 +23,6 @@ end
 input = ARGV[0]
 Compute.new(input)
 
+
+# run like so:
+#$ ruby numbers.rb 16
