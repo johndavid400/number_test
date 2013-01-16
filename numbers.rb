3e7d0e64781d2faa1b483521a03f8cefd79f27d3
johndavid400 <jd@isotope11.com>
2013-01-16 16:13:27 -0600

    added square root check

--- numbers.rb
+++ numbers.rb
@@ -2,7 +2,16 @@ class Compute
   require 'pry'
 
   def initialize(input)
-    binding.pry
+    @input = input.to_f
+    if input_is_square?
+      puts "Indeed"
+    else
+      puts "Abysmal!"
+    end
+  end
+
+  def input_is_square?
+    Math.sqrt(@input).to_i == Math.sqrt(@input)
   end
 
 end
