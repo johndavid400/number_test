097a921f30dc5d15ded0cbba3b4cdcddc1345b95
johndavid400 <jd@isotope11.com>
2013-01-17 13:40:40 -0600

    update

--- numbers.rb
+++ numbers.rb
@@ -2,28 +2,53 @@ class Compute
   require 'pry'
 
   def initialize(input)
-    @input = input.to_f
-    input_is_square? ? print_result : warn_of_failure
+    input_is_square?(input.to_f) ? print_result(input) : warn_of_failure
   end
 
-  def input_is_square?
-    Math.sqrt(@input).to_i == Math.sqrt(@input)
+  def input_is_square?(input)
+    Math.sqrt(input).to_i == Math.sqrt(input)
   end
 
   def warn_of_failure
     puts "The number you entered is not a perfect square, please try again."
   end
 
-  def print_result
-    result = (1..@input).to_a
-    row = Math.sqrt(@input).to_i
-    result.each do |n|
-      print n
-      print " "
-      if (n.to_f / row) == (n / row)
-        print "\n"
+  def print_result(input)
+    number = input.to_i
+    sqroot = Math.sqrt(input.to_f).to_i
+    sqr = sqroot
+    list = (1..input.to_i).to_a
+
+    rows = []
+    sqr.times do |s|
+      rows.push(sqr).push(sqr -= 1)
+    end
+
+    spiral = []
+    rows.each do |row|
+      row_vals = []
+      row.times do
+        row_vals.push(list.pop)
+      end
+      spiral.push(row_vals)
+    end
+    spiral.pop
+
+    final_rows = []
+    sqroot.times do |index|
+      if index == 0
+        final_rows[index] = (spiral[index])
+      elsif index == (sqroot - 1)
+      binding.pry
+        final_rows[index] = (spiral[2])
+        final_rows[index].push(spiral[1].pop)
+        binding.pry
+      else
+        final_rows[index] = (spiral[3].pop)
       end
     end
+
+    print final_rows
   end
 
 end
@@ -31,6 +56,3 @@ end
 input = ARGV[0]
 Compute.new(input)
 
-
-# run like so:
-#$ ruby numbers.rb 16
